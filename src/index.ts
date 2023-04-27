3bf920d6fa209d5db7880fbafb54a8dcd296e9ed
Eric Matala de Mazza <e.matalademazza@gmail.com>
2023-04-27 04:35:16 +0200

    feat: add reminders

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import { schedule } from "node-cron";
+import { mapping } from "./data";
 
 if (!process.env.TELEGRAM_BOT_TOKEN)
   throw new Error("TELEGRAM_BOT_TOKEN must be provided!");
@@ -10,14 +11,30 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.command("start", (ctx) =>
   ctx.reply(
-    `Hello ${ctx.chat.type === "private" ? ctx.chat.first_name : "stranger"}`
+    `Hi ${ctx.chat.type === "private" ? ctx.chat.first_name : "stranger"}`
   )
 );
 bot.command("get", (ctx) => {});
 bot.command("getall", (ctx) => {});
 bot.command("done", (ctx) => {});
 bot.command("off", (ctx) => {});
-bot.command("remind", (ctx) => {});
+
+const remind = () =>
+  Promise.all(
+    mapping
+      .filter(({ done }) => !done)
+      .map(({ roomie, duty }) =>
+        bot.telegram.sendMessage(
+          roomie.id,
+          `Hey ${roomie.name}, du hast deinen Dienst (${duty.title}) diese Woche noch nicht erledigt!`
+        )
+      )
+  );
+
+bot.command("remind", async (ctx) => {
+  await remind();
+  ctx.reply("Ich habe die anderen daran erinnert ihre Dienste zu machen.");
+});
 bot.on(message("text"), (ctx) => {});
 
 bot.launch();
@@ -25,3 +42,6 @@ bot.launch();
 // enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+// send out reminders every sunday at 6pm
+schedule("0 18 * * sun", remind);
